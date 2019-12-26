1f5d223e243f31099fbceab87c8fb9f29b22aec5
aaa <aaa@163.com>
2019-12-26 17:52:18 +0800

    爬虫

---
+++ src/main/java/com/surfilter/controller/indexController.java
@@ -0,0 +1,17 @@
+package com.surfilter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class indexController {
+
+    @RequestMapping("/index")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("/index");
+        mv.addObject( "name","hello,world!" );
+        return mv;
+    }
+
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>FreeMarker</title>
+</head>
+<body>
+${dn.contextPath}
+<h1>hello world</h1>
+<h1 style="color: red">${name}</h1>
+</body>
+<script type="text/javascript" src="${dn.contextPath}/static/js/jquery-3.4.1.min.js"/>
+</html>
